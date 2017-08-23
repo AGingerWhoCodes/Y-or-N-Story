19daaee5ee9dcfc3ec5944bdae3f6ea161fc8dd6
A Wild Potato <30586993+AGingerWhoCodes@users.noreply.github.com>
2017-08-24 10:47:48 +1200

    First try at y/n

---
+++ Cat-Y-or-N.py
@@ -0,0 +1,31 @@
+y =input("You are a cat, eat food? y/n")
+if y =="y":
+  print ("You decide to eat your cat food that your owner left out for you.")
+elif y =="n":
+  print ("You decided not to eat.")
+x =input("You feel like going outside for a walk, will you walk? y/n")
+if x =="y":
+  print ("You go through the cat flap, before seeing the warm sunshine beaming outside")
+elif x =="n":
+  print ("You stayed inside because of the chance it would be raining, but since the curtains are closed, you didn't have any idea.")
+z =input("You feel like going on an adventure in the forest, do you want to? y/n")
+if z =="y":
+  print ("Eager to go and explore the forest, you run away from home.")
+if z =="n":
+  print ("You decided to stay inside for a bit, before going out into the forest.")
+w =input("You see a sparrow and feel like hunting it, will you? y/n")
+if w =="y":
+  print ("You decided to hunt the bird.")
+if w =="n":
+  print ("You left the bird alone.")
+s =input("You feel like going home, but you see another cat walk past. Will you go home or say Hello to the cat? home/cat")
+if s =="home":
+  print ("You decided to leave the cat alone and go back home.")
+if s =="cat":
+  print ("You decided to interact with the cat, but to your suprise, it was friendly.")
+v =input("When you get home, you feel like curling up into a ball and going to sleep, will you sleep? y/n")
+if v =="y":
+  print ("Slowly, you fall asleep, waiting for the next day to come.")
+elif v =="n":
+  print ("After a little more walking inside, you fall asleep, waiting for the next day to come.")
+print ("Thanks for playing my Yes or No Story!")
